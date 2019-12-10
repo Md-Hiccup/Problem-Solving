c54c84708432336935c44882e649d97077de7de4
Md-Hiccup <ask2md@gmail.com>
2019-12-10 18:20:35 +0530

    Started Cracking The coding Interview Question

---
+++ CTCI/Array/1_is_unique.py
@@ -0,0 +1,54 @@
+""" Question
+Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?
+Hints: 
+#44     =   Try a Hash Table.
+#777    =   Could a bit vector be useful?
+#132    =   Can you solve it in O(N log N) time? What might a solution like that look like?
+"""
+
+""" Ask to Clarify Question
+1.  First, Let know, Is string is ASCII string or Unicode string.   (Assume String is ASCII)
+"""
+
+""" Solution
+1.  One solution is to create an array of boolean values, where the flag at index i indicates whether character
+i in the alphabet is contained in the string. The second time you see this character you can immediately
+return false.
+2.  We can also immediately return false if the string length exceeds the number of unique characters in the
+alphabet. After all, you can't form a string of 280 unique characters out of a 128-character alphabet.
+
+**
+It's also okay to assume 256 characters. This would be the case in extended ASCII. You should
+clarify your assumptions with your interviewer.
+**
+"""
+
+def is_unique_chars(string):
+    if len(string) > 128:
+        return False
+    
+    char_set = [False] * 128
+    
+    for i in string:
+        val = ord(i)
+        if char_set[val]:
+            return False
+        char_set[val] = True
+    
+    return True    
+
+if __name__ == '__main__':
+    string = "abcdefghia"
+    isUnique = is_unique_chars(string)
+    print(isUnique)
+
+"""
+Note:
+If we can't use additional data structures, we can do the following:
+1. Compare every character of the string to every other character of the string. This will take 0( n 2 ) time
+and 0(1) space.
+2. If we are allowed to modify the input string, we could sort the string in O(n log(n)) time and then
+linearly check the string for neighboring characters that are identical. Careful, though: many sorting
+algorithms take up extra space
+"""
---
+++ GeeksForGeeks/Learn/LinkedList/sortLinkedList.py
@@ -0,0 +1,80 @@
+"""
+To reverse a Linked List
+
+I/p =   1 -> 2 -> 3 -> Null
+
+O/p =   3 -> 2 -> 1 -> Null
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def pushFront(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def show(self):
+        temp = self.head
+        while(temp != None):
+            print(temp.data, end='->')
+            temp = temp.next
+        print(temp)
+
+    def pushBack(self, new_data):
+        new_node = Node(new_data)
+        temp = self.head
+        while (temp.next != None):
+            temp = temp.next
+        temp.next = new_node
+
+    def reverse(self):
+        curr = self.head
+        prev = None
+        next = None
+
+        while (curr != None):
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        
+        self.head = prev
+        self.show()
+
+
+    def sort(self):
+        temp = self.head
+        while (temp.next != None):
+            temp = temp.next
+
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.pushFront(4)
+    ll.pushFront(2)
+    ll.pushFront(1)
+    
+    ll.pushBack(5)
+    ll.pushBack(6)
+
+    ll.pushFront(3)
+
+    # Normal Linked List 
+    print('Normal Linked List')
+    ll.show()
+
+    # Reversed Linked List
+    print('Reverse Linked List')
+    ll.reverse()
+
+    # Sort Linked List
+    print('Sort Linked List')
+    ll.sort()

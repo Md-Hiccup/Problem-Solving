b6d50440af8c320681f787005c55f773e5e78e7c
Iqbal Ahmed <iqbal.ahmed@xische.com>
2021-01-21 10:57:14 +0400

    Solved DetectEmail

--- Java/src/hackerrank/regex/DetectEmail.java
+++ Java/src/hackerrank/regex/DetectEmail.java
@@ -2,32 +2,32 @@ package hackerrank.regex;
 
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DetectDomain {
-
-    public static void main(String args[]) {
+public class DetectEmail {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = Integer.parseInt(in.nextLine());
-        Set set = new HashSet<String>();
+
+        HashSet set = new HashSet<String>();
         for (int i = 0; i < n; i++) {
             String line = in.nextLine();
-            Pattern p = Pattern.compile("(?<=(http[s]?:\\/\\/(?:(ww2.)|(www.))?))(?!www.)(?!ww2.)([a-zA-Z\\d]+\\.[a-zA-Z\\d]*\\.?[a-zA-Z\\d]+\\.?[a-zA-Z\\d]+)");
+            Pattern p = Pattern.compile("[a-zA-Z0-9-.%+!_]*@[a-zA-Z0-9-.%+!_]+\\.[a-z]*\\.*[a-z]*\\.*[a-z]+");
             Matcher m = p.matcher(line);
             while (m.find()) {
-                set.add(m.group(4));
+                set.add(m.group(0));
             }
         }
 
-        TreeSet<String> treeSet = new TreeSet<>(set);
+        TreeSet<String> tset = new TreeSet<String>(set);
         int i = 0;
-        for (String value : treeSet) {
-            System.out.print(value);
-            if (i < treeSet.size()-1)
+        for (String str : tset) {
+            System.out.print(str);
+            if (i < tset.size() - 1) {
                 System.out.print(";");
+            }
             i++;
         }
     }

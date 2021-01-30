2880d231757f25ec4aa3e893a52fd05f49a10233
Iqbal Ahmed <iqbal.ahmed@xische.com>
2021-01-30 15:42:07 +0400

    Solved ValidateIPv4IPv6

--- Java/src/hackerrank/regex/ValidateIPv4IPv6.java
+++ Java/src/hackerrank/regex/ValidateIPv4IPv6.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 
 public class ValidateIPv4IPv6 {
     static void IPAddressValidation(String line) {
-        if (Pattern.compile("^[_.]\\d+[a-zA-Z]*_?$").matcher(line).matches()) {
+        if (Pattern.compile("(\\d|\\d{2}|1\\d\\d|2[0-5][0-6])\\.(\\d|\\d{2}|1\\d\\d|2[0-5][0-6])\\.(\\d|\\d{2}|1\\d\\d|2[0-5][0-6])\\.(\\d|\\d{2}|1\\d\\d|2[0-5][0-6])\\b").matcher(line).matches()) {
             System.out.println("IPv4");
-        } else if (Pattern.compile("^[_.]\\d+[a-zA-Z]*_?$").matcher(line).matches()) {
+        } else if (Pattern.compile("([0-9a-f]?[0-9a-f]?[0-9a-f]?[0-9a-f]:*){8}+").matcher(line).matches()) {
             System.out.println("IPv6");
         }else{
             System.out.println("Neither");
